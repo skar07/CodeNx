bb59ca5fe2bed851b35909b0c70d9f1e92833012
skar07 <sanskarpandey0@gmail.com>
2022-09-28 20:01:06 +0530

    chore: centralize and export error handlers

---
+++ server/errors/index.ts
@@ -0,0 +1,11 @@
+import { CustomAPIError } from "./custom-api";
+import { BadRequestError } from "./bad-request";
+import { UnauthenticatedError } from "./unauthenticated";
+import { NotFoundError } from "./not-found";
+
+export {
+      CustomAPIError,
+      BadRequestError,
+      UnauthenticatedError,
+      NotFoundError
+}
